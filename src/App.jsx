35ce153139502436daa44d00ed14a674fcc1bb6b
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-11-21 17:53:01 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-// import { useState } from 'react'
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import { useState } from "react";
 import "./App.css";
-import { Route, Router, RouterProvider } from "react-router-dom";
-
 function App() {
-  // const [count, setCount] = useState(0)
 
+  const[count, setCount] = useState(0)
   return (
     <>
-       <p className="text-3xl bg-red-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur provident sapiente eveniet, commodi asperiores ea nesciunt quibusdam consectetur possimus.</p>
+      <p>hellow world</p>
+    
+        <button onClick={()=> setCount(count+1)}>test</button>
+        <p>{count}</p>
 
-     
-      
+
     </>
   );
 }
