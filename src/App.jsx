c945fa711ff55bc91a1d5edcf5e901fa8cbde07e
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-11-21 16:47:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Router, RouterProvider } from "react-router-dom";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <p className="text-3xl bg-red-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur provident sapiente eveniet, commodi asperiores ea nesciunt quibusdam consectetur possimus.</p>
+
+     
+      
+    </>
+  );
+}
+
+export default App;
